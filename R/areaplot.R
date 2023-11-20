ef1847993add9a25faa2d379bb2022cb5eeebf73
Arni Magnusson <thisisarni@gmail.com>
2023-11-20 11:49:19 +1100

    Use formula method from barplot()

--- R/areaplot.R
+++ R/areaplot.R
@@ -213,31 +213,38 @@ areaplot.default <- function(x, y=NULL, prop=FALSE, rev=FALSE, add=FALSE,
 #' @export
 #' @export areaplot.formula
 
-areaplot.formula <- function(formula, data, subset, na.action=NULL, ...)
+barplot.formula <- function(formula, data, subset, na.action,
+                            horiz = FALSE, xlab = NULL, ylab = NULL, ...)
 {
-  m <- match.call(expand.dots=FALSE)
-  if(is.matrix(eval(m$data,parent.frame())))
+  if (missing(formula) || length(formula) != 3L)
+    stop("'formula' missing or incorrect")
+  m <- match.call(expand.dots = FALSE)
+  if (is.matrix(eval(m$data, parent.frame())))
     m$data <- as.data.frame(data)
-  m$... <- NULL
-  m[[1]] <- quote(model.frame)
-  if(formula[[2]] == ".")
-  {
-    ## LHS is .
-    rhs <- as.list(attr(terms(formula[-2]),"variables")[-1])
-    lhs <- as.call(c(quote(cbind), setdiff(lapply(names(data),as.name),rhs)))
-    formula[[2L]] <- lhs
-    m[[2L]] <- formula
-  }
+  m$... <- m$horiz <- m$xlab <- m$ylab <- NULL
+  m[[1L]] <- quote(stats::model.frame)
 
   mf <- eval(m, parent.frame())
-  if(is.matrix(mf[[1]]))
-  {
-    ## LHS is cbind()
-    lhs <- as.data.frame(mf[[1]])
-    areaplot.default(cbind(mf[-1],lhs), ...)
-  }
-  else
-  {
-    areaplot.default(mf[2:1], ...)
+  if (ncol(mf[-1L]) == 0L || ncol(mf[-1L]) >= 3L)
+    stop("formula must specify 1 or 2 categorical variables")
+  if (anyDuplicated(mf[-1L]))
+    stop("duplicated categorical values - try another formula or subset")
+  if (horiz) {
+    if(is.null(ylab)) ylab <- names(mf)[ncol(mf)]
+  } else
+    if(is.null(xlab)) xlab <- names(mf)[ncol(mf)]
+  if (is.matrix(mf[[1L]])) { ## LHS is cbind()
+    if (ncol(mf[-1L]) != 1L)
+      stop("formula with cbind() must specify 1 categorical variable")
+    lhs <- t(mf[[1L]])
+    colnames(lhs) <- mf[[ncol(mf)]]
+    barplot.default(lhs, horiz = horiz, xlab = xlab, ylab = ylab, ...)
+  } else {
+    if (horiz) {
+      if(is.null(xlab)) xlab <- names(mf)[1L]
+    } else
+      if(is.null(ylab))
+        ylab <- names(mf)[1L]
+    barplot.default(stats::xtabs(mf, addNA = TRUE), horiz = horiz, xlab = xlab, ylab = ylab, ...)
   }
 }
