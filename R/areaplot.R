b96dd6454d65d2e9d21e018ce1e485bebd115b5c
Arni Magnusson <thisisarni@gmail.com>
2023-11-20 12:06:05 +1100

    Adapt formula method to areaplot

--- R/areaplot.R
+++ R/areaplot.R
@@ -46,10 +46,6 @@
 #'
 #' \code{\link{areaplot-package}} gives an overview of the package.
 #'
-#' @importFrom graphics legend matplot polygon
-#' @importFrom grDevices gray.colors
-#' @importFrom stats is.ts terms time
-#'
 #' @examples
 #' areaplot(rpois(10,40))
 #' areaplot(rnorm(10))
@@ -57,7 +53,6 @@
 #' # formula
 #' areaplot(Armed.Forces~Year, data=longley)
 #' areaplot(cbind(Armed.Forces,Unemployed)~Year, data=longley)
-#' areaplot(.~Year, data=longley)
 #'
 #' # add=TRUE
 #' plot(1940:1970, 500*runif(31), ylim=c(0,500))
@@ -89,6 +84,10 @@
 #' wp <- WorldPhones[,order(colnames(WorldPhones))]
 #' areaplot(wp, col=2:8, legend=TRUE, args.legend=list(x="topleft"))
 #' areaplot(wp, col=2:8, legend=TRUE, args.legend=list(x="topleft"), rev=TRUE)
+#'
+#' @importFrom graphics legend matplot polygon
+#' @importFrom grDevices gray.colors
+#' @importFrom stats is.ts terms time
 #'
 #' @export
 
@@ -210,41 +209,41 @@ areaplot.default <- function(x, y=NULL, prop=FALSE, rev=FALSE, add=FALSE,
 }
 
 #' @rdname areaplot
+#' @importFrom stats xtabs
 #' @export
 #' @export areaplot.formula
 
-barplot.formula <- function(formula, data, subset, na.action,
-                            horiz = FALSE, xlab = NULL, ylab = NULL, ...)
+areaplot.formula <- function(formula, data, subset, na.action, xlab=NULL,
+                             ylab=NULL, ...)
 {
-  if (missing(formula) || length(formula) != 3L)
+  if(missing(formula) || length(formula) != 3L)
     stop("'formula' missing or incorrect")
-  m <- match.call(expand.dots = FALSE)
-  if (is.matrix(eval(m$data, parent.frame())))
+  m <- match.call(expand.dots=FALSE)
+  if(is.matrix(eval(m$data, parent.frame())))
     m$data <- as.data.frame(data)
-  m$... <- m$horiz <- m$xlab <- m$ylab <- NULL
-  m[[1L]] <- quote(stats::model.frame)
+  m$... <- m$xlab <- m$ylab <- NULL
+  m[[1L]] <- quote(model.frame)
 
   mf <- eval(m, parent.frame())
-  if (ncol(mf[-1L]) == 0L || ncol(mf[-1L]) >= 3L)
+  if(ncol(mf[-1L]) == 0L || ncol(mf[-1L]) >= 3L)
     stop("formula must specify 1 or 2 categorical variables")
-  if (anyDuplicated(mf[-1L]))
+  if(anyDuplicated(mf[-1L]))
     stop("duplicated categorical values - try another formula or subset")
-  if (horiz) {
-    if(is.null(ylab)) ylab <- names(mf)[ncol(mf)]
-  } else
-    if(is.null(xlab)) xlab <- names(mf)[ncol(mf)]
-  if (is.matrix(mf[[1L]])) { ## LHS is cbind()
-    if (ncol(mf[-1L]) != 1L)
+  if(is.null(xlab))
+    xlab <- names(mf)[ncol(mf)]
+  if(is.matrix(mf[[1L]]))
+  {
+    # LHS is cbind()
+    if(ncol(mf[-1L]) != 1L)
       stop("formula with cbind() must specify 1 categorical variable")
     lhs <- t(mf[[1L]])
     colnames(lhs) <- mf[[ncol(mf)]]
-    barplot.default(lhs, horiz = horiz, xlab = xlab, ylab = ylab, ...)
-  } else {
-    if (horiz) {
-      if(is.null(xlab)) xlab <- names(mf)[1L]
-    } else
-      if(is.null(ylab))
-        ylab <- names(mf)[1L]
-    barplot.default(stats::xtabs(mf, addNA = TRUE), horiz = horiz, xlab = xlab, ylab = ylab, ...)
+    areaplot.default(lhs, xlab=xlab, ylab=ylab, ...)
+  }
+  else
+  {
+    if(is.null(ylab))
+      ylab <- names(mf)[1L]
+    areaplot.default(xtabs(mf, addNA=TRUE), xlab=xlab, ylab=ylab, ...)
   }
 }
